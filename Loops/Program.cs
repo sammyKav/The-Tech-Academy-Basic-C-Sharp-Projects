f1740f5cf544bf1fd17226c98c762e3ecfb94b1f
sammyKav <skavkewi@icloud.com>
2021-08-18 10:49:43 -0700

    Csharp_loops_Assignment
    Very basic do while loop using switch method.

---
+++ Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed correctly");
+                        isGuessed = true;
+                        break;
+                    case 11:
+                        Console.WriteLine("Close but no hand grenade.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.Write("Space Ships are my favorite animal. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
