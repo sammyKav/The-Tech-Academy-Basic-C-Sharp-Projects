95a16f218f92a589fde4e0775a85e42c787b981c
sammyKav <skavkewi@icloud.com>
2021-08-18 11:14:23 -0700

    Basic String Functions

---
+++ Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string song = "Sing out Loud";
+            string artist = "Kenny Rogers";
+            string album = "Pig Farm";
+            string combine = song + " " + artist + " " + album; //concatenates strings together
+            combine.ToUpper(); //creates uppercase. 
+
+            StringBuilder sb = new StringBuilder(); //create a object of the class StringBuilder. 
+
+
+
+            sb.Append("Sentence 1");
+            sb.Append(" Sentence 2");
+            sb.Append(" Sentence 3");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
