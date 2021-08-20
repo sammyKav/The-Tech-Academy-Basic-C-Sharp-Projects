e37db6608d0c4f7461e6e2353a3f84a2e2d3b770
sammyKav <skavkewi@icloud.com>
2021-08-20 13:54:16 -0700

    Cshar_main_method_Assigment

---
+++ Main_Method/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    public class Converter
+    {
+     
+        
+    public int Addition(int param)
+        {
+            int value = param + 10;
+            return value;
+        }
+    
+    public int Addition(decimal param)
+        {
+          int value = Convert.ToInt32((param)+94);
+          return value;  
+        }
+    public int Addition(string param)
+        {
+            int num = Convert.ToInt32(param);
+            int value = num + 5 ; 
+            return value;
+        }
+    
+    }
+
+
+
+}
---
+++ Main_Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your lucky #. Must be an integer. If you're lucky number is not an int you suck!\n");
+            //Console.WriteLine("What will the input parameter type by 1: int, 2:double, 3:string");
+            Converter intreturn = new Converter();
+          
+            int param = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Returning Int: " + intreturn.Addition(param));
+            Console.WriteLine("Enter a decimal");
+            decimal param1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Returning a decimal:" + intreturn.Addition(param1));
+            Console.WriteLine("Enter a number");
+            string param2 = Console.ReadLine();
+            Console.WriteLine(intreturn.Addition(param2));
+            
+            
+
+
+            Console.ReadLine();
+            
+            
+            
+        }
+
+    }
+}
