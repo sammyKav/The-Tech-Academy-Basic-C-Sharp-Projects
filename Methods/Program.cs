f460defdef133afde8cb54e4026d27bbcc7b64ba
sammyKav <skavkewi@icloud.com>
2021-08-20 10:24:12 -0700

    Csharp_methods_Assignment

---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer");
+            int param = Convert.ToInt32(Console.ReadLine());
+            ReturnInt number = new ReturnInt();
+            number.Add(param); //calling the methods to create instances of objects 
+            number.Multiply(param);
+            number.Division(param);
+
+            Console.WriteLine("Add " + number.Addition + " divide " + number.Quotient + " multiply " + number.Product);
+            Console.ReadLine();
+        
+        }
+       
+        
+    }
+}
---
+++ Methods/ReturnInt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class ReturnInt
+    {
+        
+        public void Multiply(int param) //this method creates an Prodcut object
+        {
+            Random random = new Random();
+            int num = random.Next(0, 10);
+            Product = num * param;
+            
+        }
+
+        public void Add(int param) //this method creates an Addition object
+        {
+            Random random = new Random();
+            int num = random.Next(0, 10);
+            Addition = param + num;
+        }
+
+
+
+        public void Division(int param) //this method creates an Division object
+        {
+            Random random = new Random();
+            int num = random.Next(1, 10);
+            Quotient = param / num;
+    
+        }
+     
+        public int Product { get; set; } //create object for each method. 
+        public int Addition { get; set; }
+        public int Quotient { get; set; }
+    }
+
+}
