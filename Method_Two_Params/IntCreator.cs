02721defd973e787327dac30311ea161d1fd67db
sammyKav <skavkewi@icloud.com>
2021-08-23 10:31:39 -0700

    Csharp_method_two_param_Assignment

---
+++ Method_Two_Params/IntCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Two_Params
+{
+    class Bacteria
+    {
+        public int bacteriaGrowth(int t) { 
+           
+            int bact_pop = Convert.ToInt32(Math.Pow(2, t));//growth rate of bacteria
+            return bact_pop;
+        } 
+        
+        public int bacteriaGrowth(int t, int mgDose = 0)// add antibiotics to remove bacteria
+        {
+            int bact_pop = bacteriaGrowth(t);
+            int bact_decline = Convert.ToInt32(Math.Pow(mgDose * 100, 2));
+            int bact_remain = bact_pop - bact_decline;
+            
+            return bact_remain;
+        }
+    
+    }
+}
---
+++ Method_Two_Params/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Two_Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bacteria population = new Bacteria(); //create an object of the class
+            Console.WriteLine("You have a bacterial infection and you need antibiotics. Enter hours that have past" +
+                "since you ate a donut out of the garbage to calculate the bacteria's population. Optional: enter the mg of antibiotic" +
+                "dose to see if it'll get rid of all the bacteria. Minimize the dose to avoid complications. Negative #s represent lost" +
+                "beneficial micro-organisms");
+            Console.WriteLine("How many hours have past since consuming contaiminated food?");
+            int t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What dose in mg do you need");
+            int mgDose = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(population.bacteriaGrowth(t)); // testing it out with one parameter. 
+            Console.WriteLine(population.bacteriaGrowth(t,mgDose));//testing out with two parameters. 
+            Console.ReadLine();
+        }
+    }
+}
