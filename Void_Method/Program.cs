68e5474b6233bbfb540aba7a2e74a0d4bb2d43a0
sammyKav <skavkewi@icloud.com>
2021-08-23 11:00:45 -0700

    Cshar_void_method_Assignment

---
+++ Void_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void_Method obj = new Void_Method(); //creates an instances of a Void_Method object. 
+            obj.void_meth(num1: 15, num2: 20); //calls the method from the class. 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void_Method/Void_Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Void_Method
+    {
+        public void  void_meth(int num1, int num2)//void means it will not return a value. 
+        {
+            int value = num1 + 10; //adds 10 to value that will not be returned. 
+            Console.WriteLine(num2); //returns a argument to the console. 
+            return;
+           
+        }
+
+    }
+}
