2f1d7b873a5182b7b94883e6785e2ad815336cf4
sammyKav <skavkewi@icloud.com>
2021-08-24 11:29:32 -0700

    Csharp_abstract_classes_Assignment

---
+++ Csharp_abstract_Classes/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_abstract_Classes
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Employee " + Id + ": " + FirstName + " " + LastName);
+            base.SayName();
+        }
+    }
+}
---
+++ Csharp_abstract_Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_abstract_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };//instantiate
+            employee.SayName();//call function for abstract class 
+            Console.ReadLine();
+        }
+    }
+}
