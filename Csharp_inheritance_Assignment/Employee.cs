b744c60231740337ce620b64d56b8dfc9ded088a
sammyKav <skavkewi@icloud.com>
2021-08-24 10:03:37 -0700

    Csharp_inheritance_Assignment

---
+++ Csharp_inheritance_Assignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_inheritance_Assignment
+{
+    class Employee: Person //Employee inherits properties from Person
+    {
+        public int id { get; set; }
+        
+    }
+}
