c03155f1622b61d0b05b67bb89abca8a7bca1d0e
sammyKav <skavkewi@icloud.com>
2021-08-24 20:20:53 -0700

    Cshar_parsing_enum_Assignment

---
+++ Csharp_parsing_enum_Assignment/Day.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_parsing_enum_Assignment
+{
+
+    class Day
+    {
+        public DayofWeek DayofWeek { get; set; } //make a property of class DayofWeek. 
+    }
+        public enum DayofWeek //create class enum named DayofWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    
+}
---
+++ Csharp_parsing_enum_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_parsing_enum_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            DayofWeek day = new DayofWeek();//create new instance of day. 
+            Console.WriteLine("What's the day of the week?");
+            string currentDay = Console.ReadLine(); //get string value from user input 
+            day = (DayofWeek)Enum.Parse(typeof(DayofWeek), currentDay); // convert string value to DayofWeek type. 
+            }
+            catch (ArgumentException)//error type is Argument Exception. If that is the type then run the below code. 
+            {
+
+                Console.WriteLine("Please enter one of the following to the console.");
+                foreach ( string d in Enum.GetNames(typeof(DayofWeek))) //print list of acceptable inputs. 
+                {
+                    Console.WriteLine($" {d}");
+                }
+                       
+            }
+        Console.ReadLine();
+        }
+    }
+}
