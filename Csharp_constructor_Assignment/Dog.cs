e30bc2e6e6829f1877dd11d2c583f131b4062bf5
sammyKav <skavkewi@icloud.com>
2021-08-27 11:57:03 -0700

    Csharp_constructor_Assignment

---
+++ Csharp_constructor_Assignment/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_constructor_Assignment
+{
+    public class Dog
+    {
+        public Dog(string breed) : this(breed, "medium") { }//chaining two constructors together. 
+        public Dog(string breed, string size)// constructor
+        {
+            Breed = breed;
+            Size = size;
+        }
+        public string Breed { get; set; } //set properties
+        public string Size { get; set; }
+    }
+}
---
+++ Csharp_constructor_Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 5; //constant variable. It will not change after program has been compiled. 
+            Console.WriteLine("The magic # is " + num);
+
+             
+
+            Console.WriteLine("Enter your dog breed");
+            var breed = Console.ReadLine();//implicitly defining a variable.
+            Dog dog = new Dog(breed); //create an instance of Dog class. 
+            Console.WriteLine("Your dog has a default size of " + dog.Size + " Please go to the back of the line immediately");//this is example of linking a constructor. 
+            Console.ReadLine();
+        }
+    }
+}
