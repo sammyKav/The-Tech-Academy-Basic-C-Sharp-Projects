50caaa98cc4fc82e9afda5168214994ad82b74c7
sammyKav <skavkewi@icloud.com>
2021-09-08 15:19:46 -0700

    Basic Student Code First App

---
+++ Students/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new StudentContext())
+            {
+                Console.Write("Enter a first name for the student:");
+                var firstName = Console.ReadLine();
+                Console.Write("Enter a last name for the student:");
+                var lastName = Console.ReadLine();
+                Console.Write("Date of birth?");
+                DateTime birthDay = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Grade Name");
+                var gradeName = Console.ReadLine();
+
+                var student = new Student { FirstName = firstName, LastName = lastName, DateOfBirth = birthDay, GradeName = gradeName };
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+        }
+    }
+}
